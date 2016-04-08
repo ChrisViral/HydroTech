2d6a6586dec6f5ab3ca38dd6bb9fd9465292c585
Christophe Savard <christophe_savard@hotmail.ca>
2016-04-08 01:04:30 -0400

    Clean ManagerRendering

--- HydroTech/Managers/ManagerRendering.cs
+++ HydroTech/Managers/ManagerRendering.cs
@@ -1,8 +1,4 @@
-﻿#if DEBUG
-#define SHOW_ADD_REMOVE
-#endif
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,25 +6,28 @@ namespace HydroTech.Managers
 {
     public static class HydroRenderingManager
     {
-        private static Dictionary<int, Callback> callbackList = new Dictionary<int, Callback>();
+        #region Static Fields
+        private static readonly Dictionary<int, Callback> callbackList = new Dictionary<int, Callback>();
+        #endregion
 
+        #region Static methods
         public static void AddToPostDrawQueue(int queueSpot, Callback drawFunction)
         {
-            if (callbackList.ContainsKey(queueSpot) || callbackList.ContainsValue(drawFunction)) { throw new Exception("AddToPostDrawQueue fail: draw function (" + queueSpot + ") already added."); }
+            if (callbackList.ContainsKey(queueSpot) || callbackList.ContainsValue(drawFunction)) { throw new InvalidOperationException("AddToPostDrawQueue fail: draw function (" + queueSpot + ") already added."); }
             callbackList.Add(queueSpot, drawFunction);
             RenderingManager.AddToPostDrawQueue(queueSpot, drawFunction);
-#if SHOW_ADD_REMOVE
+#if DEBUG
             Debug.Log("Added a draw function (" + queueSpot + ")");
 #endif
         }
 
         public static void RemoveFromPostDrawQueue(int queueSpot, Callback drawFunction)
         {
-            if (!callbackList.ContainsKey(queueSpot)) { throw new Exception("RemoveFromPostDrawQueue fail: queue spot (" + queueSpot + ") not found"); }
-            if (callbackList[queueSpot] != drawFunction) { throw new Exception("RemoveFromPostDrawQueue fail: draw function not matching with queue spot (" + queueSpot + ")"); }
+            if (!callbackList.ContainsKey(queueSpot)) { throw new InvalidOperationException("RemoveFromPostDrawQueue fail: queue spot (" + queueSpot + ") not found"); }
+            if (callbackList[queueSpot] != drawFunction) { throw new InvalidOperationException("RemoveFromPostDrawQueue fail: draw function not matching with queue spot (" + queueSpot + ")"); }
             callbackList.Remove(queueSpot);
             RenderingManager.RemoveFromPostDrawQueue(queueSpot, drawFunction);
-#if SHOW_ADD_REMOVE
+#if DEBUG
             Debug.Log("Removed a draw function (" + queueSpot + ")");
 #endif
         }
@@ -37,5 +36,6 @@ namespace HydroTech.Managers
         {
             return callbackList.ContainsKey(queueSpot);
         }
+        #endregion
     }
 }
